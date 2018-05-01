e0614aa6ca158d379aca32d74d6989635507a4bb
Frederik <frederik.engels24@gmail.com>
2018-05-02 00:50:45 +0200

    make some basic rectangle typedefs

--- include/nn/stl/rectangle.hpp
+++ include/nn/stl/rectangle.hpp
@@ -1,14 +1,25 @@
 #pragma once
 
 namespace nn {
-template <typename T> struct rectangle {
+template<typename T>
+struct rectangle {
   T x;
   T y;
 
   T width;
   T height;
 
-  rectangle(const T &x, const T &y, const T &width, const T &height)
-      : x(x), y(y), width(width), height(height) {}
+  rectangle(const T& x, const T& y, const T& width, const T& height)
+      : x(x)
+      , y(y)
+      , width(width)
+      , height(height) {
+  }
 };
+
+typedef rectangle<size_t> srectangle;
+typedef rectangle<uint64_t> ui64rectangle;
+typedef rectangle<uint32_t> ui32rectangle;
+typedef rectangle<uint16_t> ui16rectangle;
+typedef rectangle<uint8_t> ui8rectangle;
 } // namespace nn
