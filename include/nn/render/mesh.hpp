814687b9eec9b9e4cf353602d826d34683c8d29c
Frederik <frederik.engels24@gmail.com>
2018-05-05 15:48:08 +0200

    rename geometry to mesh

--- include/nn/render/mesh.hpp
+++ include/nn/render/mesh.hpp
@@ -2,9 +2,12 @@
 
 #include <vector>
 
+#include "nn/render/vertex_traits.hpp"
+
 namespace nn {
 template<typename V, typename I>
-class geometry {
+class mesh {
+  static_assert(is_vertex<V>::value, "V must be a vertex type");
 public:
   using vertex_type = V;
   using index_type = I;
@@ -14,24 +17,23 @@ public:
   std::vector<index_type> indices;
 
 public:
-  geometry();
-  geometry(const std::vector<vertex_type>& vs,
-           const std::vector<index_type>& is)
+  mesh();
+  mesh(const std::vector<vertex_type>& vs, const std::vector<index_type>& is)
       : vertices(vs)
       , indices(is) {
   }
-  geometry(std::vector<vertex_type>&& vs, std::vector<index_type>&& is)
+  mesh(std::vector<vertex_type>&& vs, std::vector<index_type>&& is)
       : vertices(std::move(vs))
       , indices(std::move(is)) {
   }
   template<typename VInputIt, typename IInputIt>
-  geometry(VInputIt vfirst, VInputIt vlast, IInputIt ifirst, IInputIt ilast)
+  mesh(VInputIt vfirst, VInputIt vlast, IInputIt ifirst, IInputIt ilast)
       : vertices(vfirst, vlast)
       , indices(ifirst, ilast) {
   }
-  geometry(const geometry&) = default;
-  geometry(geometry&&) = default;
+  mesh(const mesh&) = default;
+  mesh(mesh&&) = default;
 
-  geometry& operator=(const geometry&) = default;
+  mesh& operator=(const mesh&) = default;
 };
 } // namespace nn
