5fb68c7196e11c377f1f27392a99ee687a8b7915
Frederik <frederik.engels24@gmail.com>
2018-05-06 23:05:13 +0200

    changes to the renderer interface

--- include/nn/render/2d/renderer2d.hpp
+++ include/nn/render/2d/renderer2d.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
+#include <vector>
+
 #include "nn/render/2d/renderable2d.hpp"
 
 namespace nn {
 class renderer2d {
 public:
+  struct render_info {
+
+    std::shared_ptr<renderable2d> renderable;
+    glm::mat4 transformation;
+    int z_layer;
+    float z_offset;
+  };
+
   using renderable_type = nn::renderable2d;
 
 private:
+  std::vector<render_info> m_renderables;
+
+public:
   void push(const std::shared_ptr<renderable_type>& r,
             const glm::mat4& transform, int z_layer, float z_offset);
   void flush();
