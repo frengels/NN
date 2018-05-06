63d5084a0e03689a2b15cfad51b85902b5e28a53
Frederik <frederik.engels24@gmail.com>
2018-05-07 01:37:13 +0200

    documentation for sprite class

--- include/nn/render/2d/sprite.hpp
+++ include/nn/render/2d/sprite.hpp
@@ -26,11 +26,24 @@ public:
   stclip clip;
 
 public:
+  /**
+   * a custom 2d mesh allows for more tightly packed sprites
+   */
   sprite(const std::shared_ptr<nn::texture>& tex, const stclip& rect,
          const glm::vec2& anchor, mesh_type&& mesh);
+  /**
+   * calculates mesh from clip and sets your anchor relative to the clipping
+   * points
+   */
   sprite(const std::shared_ptr<nn::texture>& tex, const stclip& rect,
          const glm::vec2& anchor);
+  /**
+   * calculates mesh from clip
+   */
   sprite(const std::shared_ptr<nn::texture>& tex, const stclip& rect);
+  /**
+   *  uses the entire texture as clip
+   */
   sprite(const std::shared_ptr<nn::texture>& tex);
   sprite(const sprite&) = default;
   sprite(sprite&&) = default;
