63ba4ac0eb397174b293e774d64cdc90be46b75e
Frederik <frederik.engels24@gmail.com>
2018-05-07 15:55:54 +0200

    more added to the base node interface

--- include/nn/sg/node_base.hpp
+++ include/nn/sg/node_base.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+#include <type_traits>
 #include <vector>
 
 #include <glm/glm.hpp>
@@ -7,17 +9,47 @@
 namespace nn {
 class node_base {
 protected:
-  std::vector<node_base> m_children;
+  node_base* m_parent{nullptr};
+  std::vector<std::unique_ptr<node_base>> m_children;
 
   bool m_visible{true};
 
-public:
+private:
+  /**
+   * only used for root component
+   */
   node_base() = default;
+
+protected:
+  /**
+   * should not directly be called, will be called through create_child(...)
+   * which takes care of proper ownership
+   */
+  node_base(node_base* parent);
+
+public:
   virtual ~node_base() = 0;
 
-  virtual const glm::mat4& world_transform();
-  virtual const glm::mat4& local_transform();
+  virtual const glm::mat4& world_transform() = 0;
+  virtual const glm::mat4& local_transform() = 0;
 
-  virtual void update();
+  virtual void update() = 0;
+
+  template<typename T, typename... Args>
+  T* create_child(Args&&... args) {
+    // make sure it's a valid node
+    static_assert(std::is_base_of_v<node_base, T>,
+                  "Created node must be derived from base!");
+
+    // create our new node and keep a reference
+    auto node_ptr = std::make_unique<T>(this, std::forward<Args>(args)...);
+    T* res = node_ptr.get();
+
+    // move the unique_ptr into the vector
+    m_children.push_back(std::move(node_ptr));
+
+    // return a non owning pointer
+    return res;
+  }
 };
 } // namespace nn
