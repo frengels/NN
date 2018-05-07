6f7600e13e9946a262cd02bbd098e4b3dffd0c80
Frederik <frederik.engels24@gmail.com>
2018-05-07 18:05:41 +0200

    expand our node a bit

--- include/nn/sg/node.hpp
+++ include/nn/sg/node.hpp
@@ -3,19 +3,30 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/quaternion.hpp>
 
+#include "nn/render/2d/transform.hpp"
+#include "nn/sg/node_base.hpp"
+
 namespace nn {
-class node {
-private:
-  glm::vec3 m_position;
-  glm::vec3 m_scale;
-  glm::quat m_rotation;
-
-  node* m_parent;
-  bool m_dirty;
-
+class node2d : public node_base {
 public:
-  node();
+  using transform_type = transform;
 
-  virtual void emplace_node();
+protected:
+  transform_type m_transform;
+
+  glm::mat4 m_cached_local;
+  /**
+   * parent x local transform
+   */
+  glm::mat4 m_cached_world;
+
+protected:
+  node2d(node_base* parent, const transform_type& transform);
+  virtual ~node2d() override;
+
+  virtual void update() override;
+
+  virtual const glm::mat4& world_transform() override;
+  virtual const glm::mat4& local_transform() override;
 };
 } // namespace nn
