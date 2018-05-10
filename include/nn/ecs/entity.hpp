76ab752e81ae9ca7947b64e875bc831fcdf14e64
Frederik <frederik.engels24@gmail.com>
2018-05-10 13:13:10 +0200

    basic entity manager

--- include/nn/ecs/entity.hpp
+++ include/nn/ecs/entity.hpp
@@ -7,28 +7,22 @@ namespace nn {
 struct entity {
 public:
   using id_type = uint32_t;
-  using version_type = uint16_t;
+  using version_type = uint32_t;
 
   static constexpr id_type INVALID_ID = std::numeric_limits<id_type>::max();
 
 public:
   id_type id{INVALID_ID};
   version_type version{0};
-  bool valid{false};
 
   entity() = default;
-  entity(id_type id, version_type v, bool valid = true)
+  entity(id_type id, version_type v)
       : id{id}
-      , version{v}
-      , valid{valid} {
-  }
-
-  operator bool() const noexcept {
-    return id != INVALID_ID && valid;
+      , version{v} {
   }
 
   bool operator==(const entity& other) const noexcept {
-    return id == other.id && version == other.version && valid == other.valid;
+    return id == other.id && version == other.version;
   }
 };
 } // namespace nn
---
+++ include/nn/ecs/entity_manager.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cassert>
+#include <queue>
+#include <tuple>
+#include <vector>
+
+#include "nn/ecs/component_store.hpp"
+#include "nn/ecs/entity.hpp"
+
+namespace nn {
+template<typename... Components>
+class entity_manager {
+  using entity_type = nn::entity;
+
+private:
+  std::vector<uint32_t> m_entities;
+  std::priority_queue<uint32_t> m_free_index;
+
+  std::tuple<component_store<Components>...> m_component_stores;
+
+public:
+  entity_manager() = default;
+
+  nn::entity create() {
+    // case where free entities are queued
+    if (!std::empty(m_free_index)) {
+      auto index = m_free_index.top();
+      m_free_index.pop();
+
+      auto version = m_entities[index];
+
+      nn::entity res = {};
+      res.id = index;
+      res.version = version;
+
+      return res;
+    }
+
+    // case where all entity slots are taken
+    else {
+      m_entities.emplace_back(0);
+      nn::entity res(std::size(m_entities) - 1, 0);
+      return res;
+    }
+  }
+
+  template<typename C>
+  void attach(const nn::entity& ent, const C& component) {
+    // crash if invalid entity
+    assert(valid(ent));
+
+    std::get<C>(m_component_stores).push(ent, component);
+  }
+
+  template<typename C, typename... Args>
+  void attach_emplace(const nn::entity& ent, Args&&... args) {
+    assert(valid(ent));
+    std::get<C>(m_component_stores).emplace(ent, std::forward<Args>(args)...);
+  }
+
+  template<typename C>
+  C* get(const nn::entity& ent) {
+    return std::get<C>(m_component_stores).get(ent);
+  }
+
+  bool valid(const nn::entity& ent) {
+    auto current_version = m_entities[ent.id];
+
+    return current_version == ent.version;
+  }
+};
+} // namespace nn
