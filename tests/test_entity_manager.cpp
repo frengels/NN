01e9c2366a4dcdb3ce7a0954c74894d170c14597
Frederik <frederik.engels24@gmail.com>
2018-05-10 13:34:52 +0200

    test case for attaching components

--- tests/test_entity_manager.cpp
+++ tests/test_entity_manager.cpp
@@ -3,11 +3,34 @@
 
 #include "nn/ecs/entity_manager.hpp"
 
-BOOST_AUTO_TEST_CASE(create_entities) {
+struct entity_manager_fixture {
   nn::entity_manager<float, int, std::string> test_manager;
+  nn::entity entity1;
+  nn::entity entity2;
+  nn::entity entity3;
 
-  auto entity1 = test_manager.create();
+  entity_manager_fixture() {
+    entity1 = test_manager.create();
+    entity2 = test_manager.create();
+    entity3 = test_manager.create();
+  }
+};
+
+BOOST_FIXTURE_TEST_SUITE(test_entity_manager, entity_manager_fixture)
+
+BOOST_AUTO_TEST_CASE(create_empty_entities) {
+
+  auto entity4 = test_manager.create();
+  BOOST_REQUIRE(test_manager.get<float>(entity4) == nullptr);
+  BOOST_REQUIRE(test_manager.get<int>(entity4) == nullptr);
+  BOOST_REQUIRE(test_manager.get<std::string>(entity4) == nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(attach_components) {
+  test_manager.attach(entity1, 73);
   BOOST_REQUIRE(test_manager.get<float>(entity1) == nullptr);
-  BOOST_REQUIRE(test_manager.get<int>(entity1) == nullptr);
+  BOOST_REQUIRE(*test_manager.get<int>(entity1) == 73);
   BOOST_REQUIRE(test_manager.get<std::string>(entity1) == nullptr);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
